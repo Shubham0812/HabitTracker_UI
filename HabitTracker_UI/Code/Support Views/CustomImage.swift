482b8e3fee79a4f5020a9e67f60df4d476e84256
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-10-15 22:33:16 +0600

    Created Custom Image View

---
+++ HabitTracker_UI/Code/Support Views/CustomImage.swift
@@ -0,0 +1,29 @@
+//
+//  CustomImage.swift
+//  HabitTracker_UI
+//
+//  Created by Shahriar Nasim Nafi on 15/10/21.
+//
+
+import SwiftUI
+
+struct CustomImage: View {
+    var imageName: String
+    var lineWidth: CGFloat
+    var borderColor: Color
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .clipShape(Circle())
+            .overlay(Circle()
+                        .stroke(borderColor, lineWidth: lineWidth))
+    }
+}
+
+struct CustomImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomImage(imageName: "boat", lineWidth: 2, borderColor: Color.yellow)
+            .padding()
+            .previewLayout(.fixed(width: 80, height: 80))
+    }
+}
