e9f8dfe6974e7f497629c1fa92de9ddd7bae342a
myawesomehub <mdcode2021@gmail.com>
2021-10-16 22:38:32 +0530

    Added Standard Spacings

---
+++ HabitTracker_UI/Utils/Spacings.swift
@@ -0,0 +1,17 @@
+//
+//  Spacings.swift
+//  HabitTracker_UI
+//
+//  Created by Mohammad Yasir on 16/10/21.
+//
+
+import SwiftUI
+
+// Mark:- Standard Spacings for iOS Devices
+public enum Spacing: Int {
+    case low = 4
+    case medium = 8
+    case standard = 16
+    case large = 24
+    case extraLarge = 32
+}
