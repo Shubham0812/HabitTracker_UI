a52091a919094e27ea0270b695d398a7db2e3f1e
Anmol Jain <defini>
2021-10-16 14:20:51 +0530

    FEAT: Added CoreData to the project

--- HabitTracker_UI/Code/Controllers/HabitTracker_UIApp.swift
+++ HabitTracker_UI/Code/Controllers/HabitTracker_UIApp.swift
@@ -10,16 +10,22 @@ import SwiftUI
 @main
 struct HabitTracker_UIApp: App {
     
+	let persistenceController = PersistenceController.shared
+	@Environment(\.scenePhase) var scenePhase
+	
     // MARK:- variables
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     // MARK:- views
     var body: some Scene {
         WindowGroup {
-            ZStack {
+            NavigationView {
                 MainView()
             }
         }
+		.onChange(of: scenePhase) { _ in
+			persistenceController.save()
+		}
     }
 }
 
